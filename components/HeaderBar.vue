dfd0522ccddf4130e398cd4a3b4219f4ec297caa
aidan946 <aidanstevens90@gmail.com>
2022-11-17 13:14:45 +1100

    Add ESLint

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,12 +1,22 @@
 <template>
-  <a-menu v-model:selectedKeys="current" mode="horizontal"  style="justify-content: center; font-weight: bold;">
-    <a-menu-item key="notes" style="justify-content: center;margin-right: 6vw">
+  <a-menu
+    v-model:selectedKeys="current"
+    mode="horizontal"
+    style="justify-content: center; font-weight: bold;"
+  >
+    <a-menu-item
+      key="notes"
+      style="justify-content: center;margin-right: 6vw"
+    >
       <template #icon>
         <BookOutlined />
       </template>
       Notes
     </a-menu-item>
-    <a-menu-item key="todos" style="justify-content: center;">
+    <a-menu-item
+      key="todos"
+      style="justify-content: center;"
+    >
       <template #icon>
         <ProjectOutlined />
       </template>
