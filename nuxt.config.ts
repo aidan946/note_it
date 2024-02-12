1c9dd446a9344dd14b7b5e07dab3acd5f56890a2
Aidan <aidanstevens90@gmail.com>
2024-02-12 20:40:48 +1100

    Fix login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: ["@nuxtjs/supabase", "@pinia/nuxt"],
   build: {
     transpile:
